352d7ce1d0ecf0af5bfba4a713421becee0be398
Alex DiLiberto <alex@alexdiliberto.com>
2014-10-12 23:47:43 -0400

    Fixed directory names in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "sublime/" \
-		--exclude ".bash_prompt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "sublime.symlink/" \
+		--exclude "atom.symlink/" --exclude ".bash_prompt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
